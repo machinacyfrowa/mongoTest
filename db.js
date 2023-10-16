af74569e2144eda13d9139578fdea71c4b18d7ff
Paweł Zawadzki <admin@machinacyfrowa.pl>
2023-10-16 13:01:36 +0200

    get one from db by fixed string

---
+++ db.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require('mongodb');
+const mongoString = "mongodb+srv://admin:w7ArSUVAh2HXjM6R@cluster0.qtyrl5o.mongodb.net/?retryWrites=true&w=majority";
+
+//funkcja tworząca połaczenie
+async function connect() {
+    const client = new MongoClient(mongoString);
+    try {
+        await client.connect();
+        return client;
+    } catch (e) {
+        console.error(e);
+    }
+}
+//ściągnij listę baz danych i wyświetl w konsoli 
+//jako parametr przyjmuje połaczenie do bazy
+async function listDB(client) {
+    let list = await client.db().admin().listDatabases();
+    //pokaż w konsoli nagłowek listy
+    console.log("Lista baz danych na serwerze:");
+    //dla każdej bazy danych na liście...
+    list.databases.forEach(database => {
+        //wyświetl napis Baza: i doklej nazwę bazy danych
+        console.log("Baza: " + database.name);
+    });
+}
+//ściagnij jeden lokal z bazy airbnb po jego nazwie
+async function getOneByName(client, name) {
+    //wez dane z bazy o nazwie "sample_airbrb", z kolekcji o nazwie "listingsAndReviews"
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne(
+        //findOne jest funkcją zaprogramowaną w sterowniku do mongodb
+        //potrzebuje ona filtra (odpowiednik WHERE w mysql zdefiniowanego w jsonie)
+        {
+            name: name
+        }
+    ); //koniec findOne()
+    //jeśli uda  się znaleźć pasujący rekord
+    if(result) {
+        return result;
+    //jeśli nie:
+    } else {
+        console.log("Nie znaleziono pasującego rekordu:");
+    }
+}
+
+function close(client) {
+    client.close();
+}
+
+module.exports = {connect, listDB, getOneByName, close}
--- index.js
+++ index.js
@@ -1,58 +1,24 @@
-const { MongoClient } = require('mongodb');
-const mongoString = "mongodb+srv://admin:w7ArSUVAh2HXjM6R@cluster0.qtyrl5o.mongodb.net/?retryWrites=true&w=majority";
+//importuj bibliotekę express
+const express = require("express");
+//przygotuj instancję expressa do użycia
+const app = express();
+//określ port na którym będzie działał express
+const port = 8000;
+//zaimportuj nasz własny moduł do obsługi bazy danych
+const db = require("./db");
 
-async function main() {
-    //stworz nowe połaczenie do bazy danych pod nazwa client używając sterownika MongoClient (alias do mongodb)
-    //i danych do połaczenia wygenerowanych z atlasa
-    const client = new MongoClient(mongoString);
+app.get("/", async function (req, res) {
+    //stwórz połączenie
+    const connection = await db.connect();
+    //wyślij zapytanie
+    const result = await db.getOneByName(connection, "Ocean View Waikiki Marina w/prkg");
+    //wyrzuć otrzymanego jsona do przeglądarki
+    res.json(result);
+    db.close(connection);
+})
 
-    try {
-        //to spróbuje się zrobić
-        //uwaga - otwieram połączenie - to może potrwać więc dajemy await
-        await client.connect();
-        //pokaż listę baz danych
-        //await listDB(client);
-        await getOneByName(client, "test");
-    } catch (e) {
-        //jeśli się wywali na twarz - wyświetl szczegóły w konsoli
-        console.error(e)
-    } finally {
-        //tak czy owak na koniec się zrobi
-        //zamykamy połaczenie
-        await client.close();
-    }
-}
-//ściągnij listę baz danych i wyświetl w konsoli 
-//jako parametr przyjmuje połaczenie do bazy
-async function listDB(client) {
-    let list = await client.db().admin().listDatabases();
-    //pokaż w konsoli nagłowek listy
-    console.log("Lista baz danych na serwerze:");
-    //dla każdej bazy danych na liście...
-    list.databases.forEach(database => {
-        //wyświetl napis Baza: i doklej nazwę bazy danych
-        console.log("Baza: " + database.name);
-    });
-}
-//ściagnij jeden lokal z bazy airbnb po jego nazwie
-async function getOneByName(client, name) {
-    //wez dane z bazy o nazwie "sample_airbrb", z kolekcji o nazwie "listingsAndReviews"
-    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne(
-        //findOne jest funkcją zaprogramowaną w sterowniku do mongodb
-        //potrzebuje ona filtra (odpowiednik WHERE w mysql zdefiniowanego w jsonie)
-        {
-            name: "Horto flat with small garden"
-        }
-    ); //koniec findOne()
-    //jeśli uda  się znaleźć pasujący rekord
-    if(result) {
-        console.log("Znaleziono pasujący rekord:");
-        console.log(result);
-    //jeśli nie:
-    } else {
-        console.log("Nie znaleziono pasującego rekordu:");
-    }
-}
 
-//uruchom funkcje main - jeśli coś pójdzie nie tak to wyrzuc bład na konsole
-main().catch(console.error);
+//uruchom wcześniej skonfigurowanego expressa
+app.listen(port, function () {
+    console.log("Express działa na porcie "+port);
+});
